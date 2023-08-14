3bf8610d763bdc16a01d3502d7ad0f4f20dbdbe1
Vet <you@example.com>
2023-08-14 14:11:31 +0200

    task finished

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if (matrix != undefined){
+    for (i = 1; i < matrix.length; i++){
+      if (i % 2 != 0){
+        matrix[i].reverse();  
+      }
+    }
+    return matrix.flat();
+  }
   return [];
 }
